fe15f0e0c1b8433cab60ca46af578251505c2d4e
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-01 20:13:22 +0530

    Create string,multi_string,character_array.py

---
+++ string,multi_string,character_array.py
@@ -0,0 +1,17 @@
+#string, multi string,character array
+print('''nishchay said 
+i am a good boy 
+nice to meet you''')
+a='chandan'
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
+print(a[6])
+# print(a[7]) #throws an index error
+#OR 'for' to print all at once character by character
+print("\nusing a for loop \n")
+for character in a:
+    print(character)#indentation is important in python
