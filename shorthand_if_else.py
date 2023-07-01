9878e2c689115a5a1e29a4cc8514cf530c10fdf5
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-02 02:12:20 +0530

    Create shorthand_if_else.py

---
+++ shorthand_if_else.py
@@ -0,0 +1,6 @@
+# shorthand if else statements in python
+
+a=20
+b=35
+# expression1 if (if)condition1 else (elif)expression2 if condition2 else (else)expression3
+print(a) if a>b else print(b) if a<b else print("a=b")
