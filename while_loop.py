4a4a7d6d91b1eb37a7e2b08b91509f349a335aad
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-01 20:26:32 +0530

    Create while_loop.py

---
+++ while_loop.py
@@ -0,0 +1,13 @@
+# while loops
+i=0
+while(i<=10):
+    print(i)
+    i=i+1
+
+i=0
+while(i>-11):
+    print(i)
+    i=i-1 #decrementing while loop
+
+# for i in range(0,-11,-1): #decrementing for loop
+#     print(i)
