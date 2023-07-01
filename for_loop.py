3bd60508b8fb547c04f87878a8d5a5e170b8bc64
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-01 20:25:59 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,14 @@
+# for loop
+a=("Chandan")
+for i in a: # to print strings as array of characters
+    print(i)
+
+colors=["red","yellow","blue","green"] 
+for color in colors: # to print elements of a list or other similar iterable objects
+    print (color)
+    for char in color: # nested for loop 
+        print(char)
+
+# range 
+for i in range('''start ='''0,'''stop ='''11,'''step ='''2): # similar to c/c++
+    print(i)
