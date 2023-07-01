a710077a6de175cf547cb0fd5cc8ff06a3e2ce49
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-01 20:07:00 +0530

    Create Typecasting.py

---
+++ Typecasting.py
@@ -0,0 +1,12 @@
+# Explicit typecasting
+a="50"
+b="3"
+print ("the value of a + b is :",a+b,"concatination is being done as both are strings" )
+c="50"
+d="3"
+print ("the value of c + d is :",int(c)+int(d),"c and d are typecasted into int")
+
+# implicit typecasting
+e=2
+f=3.46
+print("the value of e + f is :",e+f,"e is being typecasted into float by compiler")
