477fe6dba4a8cd0b3a87fad5558fc7f6b3be07b0
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-01 20:38:35 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,15 @@
+# tuples
+l=(11,45,1,2,4,6)
+print(l)
+b=(47,69,67,2,14,2,36)
+print(b.index(2)) 
+print(b.count(2)) 
+c=b 
+print(c) 
+d=(1,2,3,4,5,6,7)
+e=(80,90,100)
+print(d)
+f=d+e #for concatination of e in d
+g=e+d #for concatination of d on e
+print(f)
+print(g)
