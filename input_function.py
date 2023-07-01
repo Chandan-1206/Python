bb6a6c735e1977e649752a2726865df6c7a93640
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-01 20:08:10 +0530

    Create input_function.py

---
+++ input_function.py
@@ -0,0 +1,13 @@
+# input function (same as getline function butused as scanf or cin for python)
+a=input("enter your name : ")
+print("your name is",a)
+b=input("enter a number: ")
+c=input("enter a number: ")
+print("the sum of b and c is :",b+c) #concatination as input is taken as string
+d=input("enter a number: ")
+e=input("enter a number: ")
+print("the sum of d and e is :",int(d)+int(e))
+# OR
+f=int(input("enter a number: "))
+g=int(input("enter a number: "))
+print("the sum of f and g is :",f+g)
