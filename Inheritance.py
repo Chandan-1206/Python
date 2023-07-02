9981e177ee84b08674a2173ca9c18ffdebbb55ed
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-02 13:37:29 +0530

    Create Inheritance.py

---
+++ Inheritance.py
@@ -0,0 +1,16 @@
+# Inheritance
+class employee:
+    def _init_(self,name,id):
+        self.name=name
+        self.id=id
+    def show(self):
+        print(f"the name of employee :{self.id} is {self.name}")
+class programmers(employee): #employee is inherited in programmers
+    def showpro(self):
+        print(f"programmer is occupation of {self.name}")
+
+s1=employee("chandan",92)
+s1.show()
+s2=programmers("vanisha",87)
+s2.show()
+s2.showpro()
