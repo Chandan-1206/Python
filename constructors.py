acadd1c4e22be0551ed545710c75c90956489a43
Chandan-1206 <137202098+Chandan-1206@users.noreply.github.com>
2023-07-02 13:34:59 +0530

    Create constructors.py

---
+++ constructors.py
@@ -0,0 +1,16 @@
+# constructors
+class temp:
+# _init_ is used in python to make constructors
+    def _init_(self): # it is a default constructor as it doesn't takes arguments except self
+        print("it will be called automatically when an object is made")
+    
+a=temp()
+
+class students:
+    def _init_(self,name,age): # it is a parameterized constructor used for initializing values
+        self.name=name
+        self.age=age
+    def show(self):
+        print(f"{self.name} is of {self.age} years old")
+obj=students("froggie",18)
+obj.show()
